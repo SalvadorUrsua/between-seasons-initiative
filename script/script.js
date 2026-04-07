642410b308a99d5d10c6212bf97e4d6186927d0c
Salvador Ursua <118657557+SalvadorUrsua@users.noreply.github.com>
2026-04-08 00:15:56 +0800

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -3,7 +3,14 @@ yearEl.textContent = new Date().getFullYear();
 
 const headerCompanyName = document.querySelector(".header-company-name");
 
-const sectionHero = document.querySelector(".section-hero");
+const landingPage = document.querySelector(".landing-page");
+
+const header = document.querySelector(".section-header");
+
+const mobileButton = document.querySelector(".btn-mobile-nav");
+mobileButton.addEventListener("click", () => {
+  header.classList.toggle("mobile-main-nav");
+});
 
 const observer = new IntersectionObserver(
   (entries) => {
@@ -17,8 +24,32 @@ const observer = new IntersectionObserver(
   {
     root: null,
     threshold: 0,
-    // rootMargin: "-1px",
+    rootMargin: "-80px",
   },
 );
 
-observer.observe(sectionHero);
+observer.observe(landingPage);
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionID = href;
+      const sectionElement = document.querySelector(sectionID);
+      sectionElement.scrollIntoView({ behavior: "smooth" });
+
+      if (link.classList.contains("nav-link")) {
+        header.classList.toggle("mobile-main-nav");
+      }
+    }
+  });
+});
